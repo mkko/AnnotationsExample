8aa693b68b741b8511154b250920c9190d0b9b95
Mikko Välimäki <mkko1373@gmail.com>
2017-07-23 13:03:28 +0200

    Switch to Carthage

---
+++ Carthage/Checkouts/RxMKMapView/Example/RxMKMapView/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  RxMKMapView
+//
+//  Created by Spiros Gerokostas on 01/04/2016.
+//  Copyright (c) 2016 Spiros Gerokostas. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import RxSwift
+import RxCocoa
+import RxMKMapView
+
+class ViewController: UIViewController {
+
+    var disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        let mapView = MKMapView(frame: view.frame)
+        view.addSubview(mapView)
+
+        mapView.rx.willStartLoadingMap
+            .asDriver()
+            .drive(onNext: {
+                print("map started loaded")
+            })
+            .addDisposableTo(disposeBag)
+
+        mapView.rx.didFinishLoadingMap
+            .asDriver()
+            .drive(onNext: {
+                print("map finished loaded")
+            })
+            .addDisposableTo(disposeBag)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
